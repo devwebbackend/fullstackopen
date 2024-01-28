dfa0ac59002bad6f4912a934643e808c0abf1edb
devwebbackend1 <felfel.mohamed.dev@gmail.com>
2024-01-28 12:31:16 +0100

    parte1 exercise 1.1 fix2 final

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -15,6 +15,8 @@ const App = () => {
     <>
      <Header courses = {course} />
 <Content part ={part1} exercises={exercises1}/>
+<Content part ={part2} exercises={exercises2}/>
+<Content part ={part3} exercises={exercises3}/>
 <Total total={exercises1 +exercises2 + exercises3} />
     </>
   )
