f512879bf7ea583e2aaa81df6b4a681bed4755dc
devwebbackend1 <felfel.mohamed.dev@gmail.com>
2024-01-28 13:07:56 +0100

    part1 courseinfo exercise 1.2 add composant part into composant content

---
+++ part1/courseinfo/exercises1.2/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import Content from './Content'
+import Header from './Header'
+import Total from './Total'
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <>
+     <Header courses = {course} />
+<Content />
+
+<Total total={exercises1 +exercises2 + exercises3} />
+    </>
+  )
+}
+
+export default App
---
+++ part1/courseinfo/exercises1.2/src/Part.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Part = (props) => {
+  
+  return (
+    <><p>part {props.part} exercices {props.exercices}</p></>
+  )
+}
+
+export default Part
